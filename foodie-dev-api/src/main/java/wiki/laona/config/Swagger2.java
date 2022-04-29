1558e9542b4aaa57b5c3e716be1b14d8a4b8a81e
laona <laonane@qq.com>
2022-04-30 00:19:50 +0800

    fix(swagger2 NEP异常): Failed to start bean documentationPluginsBootstrapper

--- foodie-dev-api/src/main/java/wiki/laona/config/Swagger2.java
+++ foodie-dev-api/src/main/java/wiki/laona/config/Swagger2.java
@@ -1,5 +1,6 @@
 package wiki.laona.config;
 
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -9,14 +10,19 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
+// @EnableSwaggerBootstrapUI
 public class Swagger2 {
 
     //    http://localhost:8088/swagger-ui.html     原路径
     //    http://localhost:8088/doc.html     原路径
 
-    // 配置swagger2核心配置 docket
+    /**
+     * 配置swagger2核心配置 docket
+     */
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)  // 指定api类型为swagger2
