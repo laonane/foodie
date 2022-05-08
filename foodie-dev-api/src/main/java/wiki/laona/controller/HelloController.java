563ad90446e14836efe9e75cd77bb8177e44f13c
laona <laonane@qq.com>
2022-05-08 12:21:45 +0800

    feat(整合log4j): 添加日志打印

--- foodie-dev-api/src/main/java/wiki/laona/controller/HelloController.java
+++ foodie-dev-api/src/main/java/wiki/laona/controller/HelloController.java
@@ -1,18 +1,30 @@
 package wiki.laona.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
 
 /**
  * @author laona
  * @description hello controller
  * @date 2022-04-26 19:55
  **/
+@ApiIgnore
 @RestController
 public class HelloController {
 
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public String hello() {
+
+        logger.debug("debug: hello~");
+        logger.info("info: hello~");
+        logger.warn("warn: hello~");
+        logger.error("error: hello~");
         return "hello world!";
     }
 }
