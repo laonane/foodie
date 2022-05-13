04e7d0bb960ffefa8a2d57e4357ed75a2e97a3fa
laona <971375353@qq.com>
2022-05-13 10:45:11 +0800

    feat(映射静态资源文件): 映射本地资源文件

--- foodie-dev-api/src/main/java/wiki/laona/config/WebMvcConfig.java
+++ foodie-dev-api/src/main/java/wiki/laona/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author laona
@@ -11,10 +13,24 @@ import org.springframework.web.client.RestTemplate;
  * @since 2022-05-12 00:13
  **/
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 实现静态资源映射
+     *
+     * @param registry {@link  ResourceHandlerRegistry}
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                // 映射 swagger2
+                .addResourceLocations("classpath:/META-INF/resources/")
+                // 映射本地静态资源文件
+                .addResourceLocations("file:/E:/foodieUpload/foodie/faces/");
+    }
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder){
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
 }
