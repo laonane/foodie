87345d9a688fb025c9718f4daa7873bcca853e61
laona <971375353@qq.com>
2022-05-23 16:16:44 +0800

    feat(添加redis操作类): 添加redisOperator工具类

--- foodie-dev-api/src/main/java/wiki/laona/controller/RedisController.java
+++ foodie-dev-api/src/main/java/wiki/laona/controller/RedisController.java
@@ -1,12 +1,13 @@
 package wiki.laona.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 import wiki.laona.utils.JsonResult;
+import wiki.laona.utils.RedisOperator;
+
+import javax.annotation.Resource;
 
 /**
  * @author laona
@@ -18,24 +19,30 @@ import wiki.laona.utils.JsonResult;
 @RequestMapping("redis")
 public class RedisController {
 
-    @Autowired
-    private RedisTemplate redisTemplate;
+    // @Autowired
+    // private RedisTemplate redisTemplate;
+
+    @Resource
+    private RedisOperator redisOperator;
 
     @GetMapping("set")
     public JsonResult set(String key, String value) {
-        redisTemplate.opsForValue().set(key, value);
+        // redisTemplate.opsForValue().set(key, value);
+        redisOperator.set(key, value);
         return JsonResult.ok();
     }
 
     @GetMapping("get")
     public JsonResult get(String key) {
-        String value = (String) redisTemplate.opsForValue().get(key);
+        // String value = (String) redisTemplate.opsForValue().get(key);
+        String value = redisOperator.get(key);
         return JsonResult.ok(value);
     }
 
     @GetMapping("delete")
     public JsonResult delete(String key) {
-        redisTemplate.delete(key);
+        // redisTemplate.delete(key);
+        redisOperator.del(key);
         return JsonResult.ok();
     }
 }
